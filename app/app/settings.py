d6fcb6759cbae502cbd3d44cbeef14dd1f17f1bd
Mathius <devachievem@gmail.com>
2021-06-02 22:00:33 -0700

    add manage user endpoint

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-m3ows!^y)2_7cf%7xnmb(^7x(ndles2+vyoo@s1ui%nnoio3s=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+    'localhost'
+    ]
 
 
 # Application definition
